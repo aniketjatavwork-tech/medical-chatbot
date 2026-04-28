7b1204cc0ca5a2befdf88618b19ba7a924e304b8
Aniket Jatav <aniketjatavwork@gmail.com>
2026-04-28 18:28:48 +0530

    requirements changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+setup(
+    name="madical-chatbot",
+    version="0.1.0",
+    author=" jatav bhai",
+    author_email="jatavtesting@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+
+)
